668eb099530fc8214abe535cf0205efc86001001
LPMartinez <lpmartinez1109@gmail.com>
2020-07-21 13:14:17 -0500

    started script file to create working search bar.

---
+++ script.js
@@ -0,0 +1,30 @@
+var searchInput = document.querySelector("#search");
+var nameDiv =document.querySelector("#name");
+var searchBtn = document.querySelector("#search-btn");
+
+
+
+function nameDisplay(type, message){
+    nameDiv.textContent = message;
+    nameDiv.setAttribute("class", type)
+};
+
+searchBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    var user ={
+        collegeName: searchInput.value.trim(),
+    };
+    console.log(user)
+
+    if(user.searchInput === ""){
+        nameDisplay("error", "Search field cannot be blank")
+    }else{
+        nameDisplay("success", searchInput)
+    }
+
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    var lastName = JSON.parse(localStorage.getItem("user"));
+
+})
